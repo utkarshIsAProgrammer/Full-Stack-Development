fd3abd2d46af0f79a27a2075446ff27549a98ced
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-11-30 01:47:06 +0530

    start learning javascript

---
+++ Phase-1-Foundations/JavaScript/02-javascript-basics/script.js
@@ -0,0 +1,56 @@
+// This is a single line comment.
+
+/* 
+This is 
+a multiline
+comment.
+*/
+
+// console message types.....
+console.log("Hello World!");
+console.warn("Warning Message!");
+console.error("Error Message!");
+
+// template literals.....
+console.log(`4 + 5 = ${4 + 5}`);
+
+// user input.....
+const prompt = require("prompt-sync")();
+const name = prompt("Enter your name: ");
+console.log(name);
+
+// primitive data types.....
+// 1. string
+console.log("Double quoted string.");
+console.log(`Single quoted string.`);
+console.log("Backticks string.");
+
+// 2. boolean
+console.log(true);
+console.log(false);
+
+// 3. number
+console.log(55);
+console.log(5.5);
+
+// 4. undefined
+console.log(undefined);
+
+// 5. null
+console.log(null);
+
+// variables.....
+var redColor = "red"; // GLOBAL SCOPED
+console.log(redColor);
+
+redColor = "#ff0000";
+console.log(redColor);
+
+let greenColor = "green"; // BLOCK SCOPED
+console.log(greenColor);
+
+greenColor = "#00ff00";
+console.log(greenColor);
+
+const blackColor = "#000"; // BLOCK SCOPED
+console.log(blackColor);
