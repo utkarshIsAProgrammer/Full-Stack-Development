60dce756ac3005fcd830da489a5c0d7e6031f5e1
Utkarsh Yadav <utkarshprogrammer2005@gmail.com>
2025-11-30 07:50:58 +0530

    learn switch statements

---
+++ Phase-1-Foundations/JavaScript/04-switch-statements/script.js
@@ -0,0 +1,28 @@
+let day = "Sunday";
+
+switch (day) {
+	case "Monday":
+		console.log("Week Day!");
+		break;
+	case "Tuesday":
+		console.log("Week Day!");
+		break;
+	case "Wednesday":
+		console.log("Week Day!");
+		break;
+	case "Thursday":
+		console.log("Week Day!");
+		break;
+	case "Friday":
+		console.log("Week Day!");
+		break;
+	case "Saturday":
+		console.log("Weekend Day!");
+		break;
+	case "Sunday":
+		console.log("Weekend Day!");
+		break;
+	default:
+		console.log("Check you input, something is wrong there.....");
+		break;
+}
